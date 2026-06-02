18da1cbd4fc8f6926f90f64b9845575d309f7e54
gokuyash <brothers14jan@gmail.com>
2026-06-02 17:36:25 +0530

    Create BMI.py
    Programming to create a Body Mass Index in a simple calculation using a person's height and weight .
    Formula is BMI=kg/m*m

---
+++ BMI.py
@@ -0,0 +1,10 @@
+#to calculate BMI=kg/m square
+weight_in_kg=float(input("Enter weight in kg: "))
+height_in_meter=float(input("Enter height in meters: "))
+bmi=weight_in_kg/(height_in_meter*height_in_meter)
+print("BMI is : ",bmi) 
+                 #output
+Enter weight in kg: 10 
+Enter height in meters: 5
+BMI is :  0.4
+
