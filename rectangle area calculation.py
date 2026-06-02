7eec28f16ff4168f70819b283b4647b1a4a633d2
gokuyash <brothers14jan@gmail.com>
2026-06-02 11:42:41 +0530

    Add rectangle area calculation script
    This script calculates the area of a rectangle based on user input for length and breadth, and prints the specifications along with the calculated area.

---
+++ rectangle area calculation.py
@@ -0,0 +1,14 @@
+#to print length and breadth of a rectangle and calculate its area
+length=float(input("Enter length of the rectangle::"))
+breadth=float(input("Enter the breadth of the rectangle::"))
+area=length*breadth
+print("Rectangle specifications")
+print("Length=",length,end=' ')
+print("breadth=",breadth)
+print("Area=",area)
+                         #output
+Enter length of the rectangle::100.5 #enter any value
+Enter the breadth of the rectangle 990 #enter any value
+Rectangle speacifications
+Length= 100.5 breadth= 990.0 #your given values
+Area= 99495.0 #final output of your answer
