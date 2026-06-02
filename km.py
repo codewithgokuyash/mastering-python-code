7a1858b3f9940abe8e6dfd8ace07eaa1f3c39adc
gokuyash <brothers14jan@gmail.com>
2026-06-02 17:49:03 +0530

    Create km.py
    writing a code to program to input a value in kilometers and converts in miles

---
+++ km.py
@@ -0,0 +1,9 @@
+km=int(input("Enter kilometers: "))
+miles=km*0.621371
+print("Kilometers :",km)
+print("Miles :",miles)
+                 #output
+
+Enter kilometers: 100
+Kilometers : 100
+Miles : 62.137100000000004
