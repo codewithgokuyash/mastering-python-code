daffbe3262621372f3836931f57ffdd17d08ecc1
gokuyash <brothers14jan@gmail.com>
2026-06-02 17:54:45 +0530

    Create swapping inputs.py
    Writing a program to input two number and swap them

---
+++ swapping inputs.py
@@ -0,0 +1,10 @@
+n1=int(input("Enter number1 ="))
+n2=int(input("Enter number2 ="))
+print("Original numbers :",n1,n2)
+n1,n2=n2,n1
+print("After swapping :",n1,n2)
+         #output
+Enter number1 =100
+Enter number2 =200
+Original numbers : 100 200
+After swapping : 200 100
