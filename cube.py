efcb2ae2788df2c2bb8d9efc1628e18eaf6e7ca6
gokuyash <brothers14jan@gmail.com>
2026-06-02 17:43:53 +0530

    Create cube.py
    creating a code to input a number and print its cube

---
+++ cube.py
@@ -0,0 +1,8 @@
+num=int(input("Enter any number: "))
+cube=num*num*num
+print("Number is",num)
+print("Its cube is ",cube)
+           #output
+Enter any number: 100
+Number is 100
+Its cube is  1000000
