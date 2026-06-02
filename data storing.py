25aed57dcf7cbb9a465140c34024712ac385324e
gokuyash <brothers14jan@gmail.com>
2026-06-02 11:10:03 +0530

    Add basic user input for name and age

---
+++ data storing.py
@@ -0,0 +1,8 @@
+name=input("Enter your name") #statement
+Enter your name goku #output
+age=input("Enter your age") #statement
+Enter your age 17 #output
+name #statement
+' goku' #output
+age #statement
+' 17' #output
