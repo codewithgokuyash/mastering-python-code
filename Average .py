bc967eacd68deded8aacb81ec43b33c7470d8033
gokuyash <brothers14jan@gmail.com>
2026-06-03 17:19:23 +0530

    Create Average .py
    Writing a program to find average of three numbers

---
+++ Average .py
@@ -0,0 +1,13 @@
+x=int(input("Enter any value 1 : "))
+y=int(input("Enter any value 2 : "))
+z=int(input("Enter any value 3 : "))
+print("Three numbers are : ",x,y,z)
+avg=(x+y+z)/3
+print("Average of given number is : ", avg)
+
+   #output
+#Enter any value 1 : 1999
+#Enter any value 2 : 1200
+#Enter any value 3 : 9090
+#Three numbers are :  1999 1200 9090
+#Average of given number is :  4096.333333333333
