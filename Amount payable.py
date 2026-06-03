673aabcca48e292baf2e330fdf85005325f70aaa
gokuyash <brothers14jan@gmail.com>
2026-06-03 17:41:05 +0530

    Create Amount payable.py
    Writing a program to calculate the amount payable if the money has been lent on simple interest Principle or money lent = P, rate of interest =R% per annum and Time = T years

---
+++ Amount payable.py
@@ -0,0 +1,11 @@
+P=float(input("Enter money lent: "))
+R=float(input("Enter rate of interest:"))
+T=float(input("Enter time in years :"))
+SI=(P*R*T)/100
+amount_payable= P+SI
+print("Amount payable is Rs.",amount_payable)
+    #output
+#Enter money lent: 100000
+#Enter rate of interest:2
+#Enter time in years :2
+#Amount payable is Rs. 104000.0
