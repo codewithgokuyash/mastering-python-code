d38233b720ea724bf80c424c86dc3c7c2f91f51c
gokuyash <brothers14jan@gmail.com>
2026-06-03 12:23:31 +0530

    Create All arithmetic operation.py
    writing a program to enter two integers and perform all arithmetic operations on them

---
+++ All arithmetic operation.py
@@ -0,0 +1,25 @@
+x=int(input("Enter any 1 value for the calculation: "))
+y=int(input("Enter any 2 value for the calculation: "))
+summ=x+y
+diff=x-y
+mult=x*y
+div=x/y
+mod=x%y
+print("Numbers are : ",x,y)
+print("Sum = ",summ)
+print("Difference = ",diff)
+print("Product = ",mult)
+print("Divison = ",div)
+print("Modulus = ",mod)
+print("All your answer are printed successfully")
+
+                #output
+Enter any 1 value for the calculation: 100
+Enter any 2 value for the calculation: 10
+Numbers are :  100 10
+Sum =  110
+Difference =  90
+Product =  1000
+Divison =  10.0
+Modulus =  0
+All your answer are printed successfully
