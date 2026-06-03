50beea276ee84780e6b283fa78e31ae6b3268575
gokuyash <brothers14jan@gmail.com>
2026-06-03 11:38:27 +0530

    Add Celsius to Fahrenheit temperature converter
    writing a program to obtain temperature in celsius and converts it into Fahrenheit using formula

---
+++ Temperature converter.py
@@ -0,0 +1,10 @@
+#celsius to Fahrenheit
+Cels=float(input("Enter temperature in Celsius: "))
+print("Temperature in celcius is :: ",Cels)
+Fahr=Cels*9/5+32
+print("Temperature is Fahrenheit is : ", Fahr)
+
+               #output
+Enter temperature in Celsius: 1000
+Temperature in celcius is ::  1000.0
+Temperature is Fahrenheit is :  1832.0
