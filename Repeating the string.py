5fb4e744890835b3bbc4aa76566a475a3735ed30
gokuyash <brothers14jan@gmail.com>
2026-06-03 17:50:04 +0530

    Create Repeating the string.py

---
+++ Repeating the string.py
@@ -0,0 +1,6 @@
+n=int(input("How many times ?"))
+print("Happy Birthday Python may god bless you "*n)
+
+         #output
+#How many times ?10
+#Happy Birthday Python may god bless you Happy Birthday Python may god bless you Happy Birthday Python may god bless you Happy Birthday Python may god bless you Happy Birthday Python may god bless you Happy Birthday Python may god bless you Happy Birthday Python may god bless you Happy Birthday Python may god bless you Happy Birthday Python may god bless you Happy Birthday Python may god bless you 
