0b548fc07943c87883d429213639907a20c4b032
gokuyash <brothers14jan@gmail.com>
2026-06-06 20:23:05 +0530

    Add Average.py to calculate the average of three numbers

---
+++ Average.py
@@ -0,0 +1,9 @@
+# Average of 3 numbers
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+c = float(input("Enter third number: "))
+
+average = (a + b + c) / 3
+
+print("Average =", average)
