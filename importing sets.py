e45892086cf73fc2c69759c6d384b83404f063db
gokuyash <brothers14jan@gmail.com>
2026-06-08 16:20:55 +0530

    Add random number generation and set creation
    Writing a code fragment to generate three random integers In the range 10,70 with a step of 13 and create a set with these integer

---
+++ importing sets.py
@@ -0,0 +1,10 @@
+import random
+num1 =random.randrange(10,70,13)
+num2 =random.randrange(10,70,13)
+num3 =random.randrange(10,70,13)
+set1={num1,num2,num3}
+print("Random integer in the range 10..70,step_value13:",num1,num2,num3)
+print("Set created  ::",set1)
+             #output 
+Random integer in the range 10..70,step_value13: 10 36 49
+Set created  :: {49, 10, 36}
