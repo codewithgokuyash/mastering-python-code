4f292098380e3f7556e54eb99b9ea67c200f2c2f
gokuyash <brothers14jan@gmail.com>
2026-06-08 15:54:31 +0530

    Fix output formatting in random.py

---
+++ import random.py
@@ -0,0 +1,15 @@
+>>> import random
+>>> print(random .randint(3,10)-3)
+5 #output
+>>> print(random .randint(3,10)-3)
+0 #output
+>>> print(random .randint(3,10)-3)
+6 #output
+>>> print(random .randint(3,10)-3)
+2 #output
+>>> print(random .randint(3,10)-3)
+7 #output
+>>> print(random .randint(3,10)-3)
+5 #output
+>>> print(random .randint(3,10)-3)
+5 #output
