e633e98481bbdb6f89d7c4435a81c13c4f5c39b3
gokuyash <brothers14jan@gmail.com>
2026-06-08 16:11:34 +0530

    Add script to calculate average of two random numbers
    Writing a code fragment to generate two random integers between 450 and 950. And printing those numbers along with their  average

---
+++ Average math.py
@@ -0,0 +1,9 @@
+import random
+num1=random.randint(450,950)-450
+num2=random.randint(450,950)-450
+avg=(num1+num2)/2
+print("Random integer in the range 450 to 950 :",num1,num2)
+print("Their average :",avg)
+      #output 
+Random integer in the range 450 to 950 : 388 296
+Their average : 342.0
