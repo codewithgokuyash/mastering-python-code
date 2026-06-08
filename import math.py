93c1f99e952a713e3d719fa8984652f97eea220d
gokuyash <brothers14jan@gmail.com>
2026-06-08 16:04:58 +0530

    Add script to generate random number and its ceiling
    Writing a code fragment to generate a random floating number between 45.0 and 95.0 and printing this number along with its nearest integer greater than it

---
+++ import math.py
@@ -0,0 +1,11 @@
+import random
+import math
+fnum=random.random()*(95-45)+45
+inum=math.ceil(fnum)
+print("Random numbers between 45..95:")
+print(fnum)
+print("Nearest higher integer :",inum)
+    #output 
+Random numbers between 45..95:
+66.79265774454099
+Nearest higher integer : 67
