cd706f681d5831d3e58d9996d2b8c552acc3cc21
gokuyash <brothers14jan@gmail.com>
2026-06-09 15:24:48 +0530

    Create Largest number.py
    Writing a program to accept three integers and print the largest of the three . Make use of only if statement.

---
+++ Largest number.py
@@ -0,0 +1,15 @@
+x=y=z=0
+x=float(input("Enter first number : "))
+y=float(input("Enter second number : "))
+z=float(input("Enter third number : "))
+max=x
+if y>max:
+   max=y
+if z>max:
+  max=z
+print("Largest number is ",max)
+      #output
+Enter first number : 1000
+Enter second number : 200
+Enter third number : 800
+Largest number is  1000.0
