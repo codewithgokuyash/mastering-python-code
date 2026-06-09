a948a5fc73f0f0e998b532368275d259f6c78209
gokuyash <brothers14jan@gmail.com>
2026-06-09 15:18:42 +0530

    Add Even number checker in Even number.py
    Writing a program that takes a number and checks whether the given number Is odd or even

---
+++ Even number.py
@@ -0,0 +1,8 @@
+num=int(input("Enter an integer:"))
+if num % 2==0:
+    print(num,"is EVEN number.")
+else:
+    print(num,"is ODD number.")
+       #output    
+Enter an integer:1000
+1000 is EVEN number.
