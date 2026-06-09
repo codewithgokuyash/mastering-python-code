e0dfd3d37e704b3ddb5168ba09c6dadf42ece887
gokuyash <brothers14jan@gmail.com>
2026-06-09 15:58:47 +0530

    Create Divisible.py
    Writing a program to test the divisibility of a number with another number

---
+++ Divisible.py
@@ -0,0 +1,11 @@
+number1=int(input("Enter first number : "))
+number2=int(input("Enter second number : "))
+remainder=number1%number2
+if remainder==0:
+   print(number1,"is divisible by",number2)
+else:
+   print(number1,"is not divisible by ",number2)
+     #output
+Enter first number : 2000
+Enter second number : 20
+2000 is divisible by 20
